20df3e1a3278753f320df6669bf2b600a1927d39
francescobassi2002 <50515728+francescoBassi2002@users.noreply.github.com>
2020-11-17 17:50:39 +0100

    Update RSA.py

--- Sistemi_quinta/crittografia/RSA.py
+++ Sistemi_quinta/crittografia/RSA.py
@@ -1,10 +1,11 @@
 import funzioni_varie
 
 def cifra(messaggio , c , n):
-    return (messaggio^c) % n
+    print(f"({messaggio}^{c}) mod {n}")
+    return (messaggio**c) % n
 
 def decifra(messaggio_cifrato , d , n):
-    return (messaggio_cifrato^d) % n
+    return (messaggio_cifrato**d) % n
 
 
 messaggio_da_cifrare = int(input("Inserisci il messaggio da cifrare: "))
@@ -21,8 +22,8 @@ chiave_pubblica_c = funzioni_varie.rsa_calcolo_c(chiave_privata_m)
 
 chiave_privata_d = funzioni_varie.rsa_calcolo_d(chiave_pubblica_c, chiave_privata_m)
 
-print(f"le chiavi private sono: {chiave_privata_p} , {chiave_privata_q} , {chiave_privata_m} , {chiave_privata_d}")
-print(f"le chiavi pubbliche sono: {chiave_pubblica_c} , {chiave_pubblica_n}")
+print(f"le chiavi private sono: p {chiave_privata_p} , q {chiave_privata_q} , m {chiave_privata_m} , d {chiave_privata_d}")
+print(f"le chiavi pubbliche sono: c {chiave_pubblica_c} , n {chiave_pubblica_n}")
 
 messaggio_cifrato = cifra(messaggio_da_cifrare, chiave_pubblica_c, chiave_pubblica_n)
 
